cea1bb6ceddef5c08b41c0a19e8cd3fdd7f613a3
ezio <iixelite@gmail.com>
2019-06-08 16:40:07 +0200

    Stato attuale:
    server in grado di gestire richieste di client. Ad ogni richiesta, un clinet riceve una 'MateriaPlus' con nome materia, voto, crediti, rarità, tempo di emissione della materia da parte del server, tempo di acquisizione della mteria da parte del server.
    In tal modo ogni clinet (lato mobile) dovrà calcolarti il tempo rimanente della materia., fatto per alleggerire il carico di lavore del server.
    
    SERVONO:
    
    -Funzione di Giovanni per ottenere la 'MateriaPlus'.
    
    AVVISO:
    
    -Giova usa come attributi e metodi quelli definiti in MateriaPlus, se vuoi puoi aggiungerne altri.

---
+++ src/server/MateriaPlusUpdater.java
@@ -0,0 +1,65 @@
+package server;
+
+import com.google.gson.Gson;
+import controller.MockMateriaPlusCalculator;
+import entities.MateriaPlus;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+//Thread che deve essere lanciato all'avvio del master thread,
+//e che ogni 'UPDATE_AFTER_MINUTES' dovra' ''calcolare'' un nuovo voto
+public class MateriaPlusUpdater implements Runnable {
+
+    private static final int UPDATE_AFTER_MINUTES = 1;
+
+    @Override
+    public void run() {
+
+        MateriaPlus materia;
+        String emissionTime;
+
+        try{
+            while(true) {
+
+                materia = MockMateriaPlusCalculator.materiaCalculator();
+
+                //setto il rempo di emissione alla materia
+                emissionTime = getCurrentTimeUsingCalendar();
+                materia.setEmissionTime(emissionTime);
+
+                //aggiorna la materia solo se nessun altro client la sta leggendo
+                MasterServer.readWriteLock.writeLock().lock();
+
+                MasterServer.materiaPlus = materia;
+
+                //unlock
+                MasterServer.readWriteLock.writeLock().unlock();
+
+                System.out.println("Json Materia generated:\n" + materia.toString());
+                Thread.sleep(1000 * 60 * MateriaPlusUpdater.UPDATE_AFTER_MINUTES);
+            }
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+
+            //se il timer va giu', devo riavviarlo immediatamente
+            //per garantire un minimo di robustezza
+            MateriaPlusUpdater timer = new MateriaPlusUpdater();
+            Thread t = new Thread(timer);
+            t.start();
+        }
+    }
+
+    public static String getCurrentTimeUsingCalendar() {
+
+        Calendar cal = Calendar.getInstance();
+        Date date=cal.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        String formattedDate = dateFormat.format(date);
+        return formattedDate;
+
+    }
+}
